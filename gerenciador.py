7edbc736725c723c1232eb2c40d722e2af34da49
BRUNOSR-DEV <bruno77_sr@hotmail.com>
2025-06-21 11:24:18 -0300

    sistema de login pronto

--- gerenciador.py
+++ gerenciador.py
@@ -36,6 +36,16 @@ import customtkinter as ctk
 
 ctk.set_appearance_mode('dark')
 
+#Criação das funções de funcionalidades
+def validar_login():
+    usuario = campo_usuario.get()
+    senha = campo_senha.get()
+
+    if usuario == 'bruno' and senha == '123456':
+        resultado_login.configure(text='Login feito com sucesso', text_color='green')
+    else:
+        resultado_login.configure(text='Login Incorreto!', text_color='red')
+
 #criação de tela inicial
 
 app = ctk.CTk()
@@ -53,15 +63,22 @@ campo_usuario = ctk.CTkEntry(app, placeholder_text='Digite seu usuário')
 campo_usuario.pack(pady=10)
 
 #Label - Senha
-label_senha = ctk.CTkLabel(app, text='Senha')
+label_senha = ctk.CTkLabel(app, text='Senha', )
 label_senha.pack()
 
 #entry - Campo de usuário
-campo_senha = ctk.CTkEntry(app, placeholder_text='Digite sua senha')
+campo_senha = ctk.CTkEntry(app, placeholder_text='Digite sua senha', show='*')
 campo_senha.pack(pady=10)
 #Button
-botao = ctk.CTkButton(app, text='Login')
-botao.pack(pady=10)
+botao_login = ctk.CTkButton(app, text='Login', command=validar_login)
+botao_login.pack()
+
+#campo feedback de login
+resultado_login = ctk.CTkLabel(app, text='')
+resultado_login.pack(pady=10)
+
+
+
 
 #iniciar a aplicação
 app.mainloop()
